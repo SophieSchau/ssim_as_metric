9359abec743d02680685bdf6784f168fa91a246c
Sophie <schauman@stanford.edu>
2023-01-13 13:52:41 -0800

    edits for presentation

--- ssim_in_matlab.m
+++ ssim_in_matlab.m
@@ -24,7 +24,7 @@ disp('Scaled by max:')
 full_ssim_1 = ssim(GT/max(GT(:)),abs(recon_full_ifft)/max(abs(recon_full_ifft(:)))) ;
 disp(['Full : ' , num2str(full_ssim_1)])
 
-[us1_ssim_1, S] = ssim(GT/max(GT(:)),abs(recon_us1_ifft)/max(abs(recon_us1_ifft(:)))) ;
+us1_ssim_1 = ssim(GT/max(GT(:)),abs(recon_us1_ifft)/max(abs(recon_us1_ifft(:)))) ;
 disp(['Undersampled 1 : ' , num2str(us1_ssim_1)])
 
 us2_ssim_1 = ssim(GT/max(GT(:)),abs(recon_us2_ifft)/max(abs(recon_us2_ifft(:)))) ;
@@ -72,9 +72,12 @@ disp(['Undersampled 2 : ' , num2str(us2_ssim_3)])
 us3_ssim_3 = ssim(uint16(GT),uint16(abs(recon_us3_ifft))) ;
 disp(['Undersampled 3 : ' , num2str(us3_ssim_3)])
 
-us4_ssim_3 = ssim(uint16(GT),uint16(abs(recon_us4_ifft))) ;
+[us4_ssim_3, S] = ssim(uint16(GT),uint16(abs(recon_us4_ifft))) ;
 disp(['Undersampled 4 : ' , num2str(us4_ssim_3)])
+
+[us4_ssim_3_m, S_m] = ssim(uint16(GT.*mask),uint16(abs(recon_us4_ifft).*mask)) ;
+disp(['Undersampled 4 (masked) : ' , num2str(us4_ssim_3_m)])
 
 disp('--------------------------------------------------')
 
-save('data/ssim_matlab.mat','full_ssim_0','full_ssim_1','full_ssim_2','full_ssim_3', 'us1_ssim_0', 'us1_ssim_1', 'us1_ssim_2', 'us1_ssim_3', 'us2_ssim_0', 'us2_ssim_1', 'us2_ssim_2','us2_ssim_3','us3_ssim_0', 'us3_ssim_1', 'us3_ssim_2','us3_ssim_3','us4_ssim_0', 'us4_ssim_1', 'us4_ssim_2','us4_ssim_3','S')
+save('data/ssim_matlab.mat','full_ssim_0','full_ssim_1','full_ssim_2','full_ssim_3', 'us1_ssim_0', 'us1_ssim_1', 'us1_ssim_2', 'us1_ssim_3', 'us2_ssim_0', 'us2_ssim_1', 'us2_ssim_2','us2_ssim_3','us3_ssim_0', 'us3_ssim_1', 'us3_ssim_2','us3_ssim_3','us4_ssim_0', 'us4_ssim_1', 'us4_ssim_2','us4_ssim_3','us4_ssim_3_m','S', 'S_m')
